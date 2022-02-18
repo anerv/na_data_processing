8896f7b1834a316b1aaa4a90673ecb59412872bb
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-02-18 18:14:04 +0100

    new func added

--- src/geometric_functions.py
+++ src/geometric_functions.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import numpy as np
 
 def get_geom_diff(geom1, geom2):
 
@@ -18,3 +19,24 @@ def get_geom_diff(geom1, geom2):
     geom_diff = geodf1.difference(geodf2).values[0]
 
     return geom_diff
+
+
+def get_angle(linestring1, linestring2):
+    '''
+    Function for getting the smallest angle between two lines, without considering direction of lines
+    I.e. is the angle larger than 90, it is instead expressed as 180 - org angle
+    '''
+
+    arr1 = np.array(linestring1.coords)
+    arr1 = arr1[1] - arr1[0]
+
+    arr2 = np.array(linestring2.coords)
+    arr2 = arr2[1] - arr2[0]
+
+    angle = np.math.atan2(np.linalg.det([arr1,arr2]),np.dot(arr1,arr2))
+    angle_deg = abs(np.degrees(angle))
+
+    if angle_deg > 90:
+        angle_deg = 180 - angle_deg
+
+    return angle_deg
