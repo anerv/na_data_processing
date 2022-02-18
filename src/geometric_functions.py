2aacb95c6c3fa0bda04f8544c8b19e8da35b5224
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-02-18 16:20:49 +0100

    function for finding diff bt geoms

---
+++ src/geometric_functions.py
@@ -0,0 +1,18 @@
+def get_geom_diff(geom1, geom2):
+
+    '''
+    Function for getting the geometric difference between two geometries
+    Input geometries are shapely geometries - e.g. LineStrings.
+    The resulting difference is also returned as a shapely geometry.
+    '''
+
+    geoms1 = [geom1]
+    geoms2 = [geom2]
+
+    geodf1 = gpd.GeoDataFrame(geometry=geoms1)
+
+    geodf2 = gpd.GeoDataFrame(geometry=geoms2)
+
+    geom_diff = geodf1.difference(geodf2).values[0]
+
+    return geom_diff
