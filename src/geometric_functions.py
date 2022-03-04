3adb77e4d3cd418f6de231cf97b30d7932d17ffc
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-03-04 16:10:05 +0100

    added documentation

--- src/geometric_functions.py
+++ src/geometric_functions.py
@@ -5,14 +5,51 @@ from scipy.spatial.distance import directed_hausdorff
 from shapely.ops import nearest_points, split, linemerge, snap
 from shapely.geometry import Point, MultiPoint, LineString
 
+
+# TODO: Add tests!
+
 ############
+
+"""
+    Counts the number of times a line occurs. Case-sensitive.
+
+    Parameters
+    ----------
+    f: file
+        the file to scan
+    line: str
+        the line to count
+
+    Returns
+    -------
+    int
+        the number of times the line occurs.
+    """
+
+######
 
 def get_geom_diff(geom1, geom2):
 
     '''
-    Function for getting the geometric difference between two geometries
-    Input geometries are shapely geometries - e.g. LineStrings.
-    The resulting difference is also returned as a shapely geometry.
+    Function for getting the geometric difference between two geometries.
+    Input geometries are Shapely geometries - e.g. LineStrings.
+    The resulting difference is also returned as a Shapely geometry.
+    Makes use of Geopandas difference function.
+    The order in which the geometries are passed to the function does not matter.
+
+    Parameters
+    ----------
+    geom1: Shapely geometry
+        The first geometry
+
+
+    geom2: Shapely geometry
+        The second geometry
+
+    Returns
+    -------
+    geom_diff
+        The geomtric difference in the form of a Shapely geometry.
     '''
 
     geoms1 = [geom1]
@@ -28,9 +65,22 @@ def get_geom_diff(geom1, geom2):
 
 
 def get_angle(linestring1, linestring2):
+
     '''
-    Function for getting the smallest angle between two lines, without considering direction of lines
-    I.e. is the angle larger than 90, it is instead expressed as 180 - org angle
+    Function for getting the smallest angle between two lines.
+    Does not consider the direction of lines: I.e. is the angle larger than 90, it is instead expressed as 180 - original angle.
+
+    Parameters
+    ----------
+    linestring1: Shapely geometry
+
+    linestring2: Shapely geometry
+
+    Returns
+    -------
+    angle_deg: float
+        angle expressed in degrees
+
     '''
 
     arr1 = np.array(linestring1.coords)
@@ -48,15 +98,98 @@ def get_angle(linestring1, linestring2):
     return angle_deg
 
 
+def get_hausdorff_dist(osm_edge, ref_edge):
+
+    '''
+    Computes the Hausdorff distance (max distance) between two LineStrings.
+
+    Parameters
+    ----------
+
+    osm_edge: Shapely LineString
+        The first geometry to compute Hausdorff distance between.
+
+    ref_edge: Shapely LineString
+        Second geometry to be used in distance calculation.
+
+    Returns
+    -------
+    hausdorff_dist: float
+        The Hausdorff distance.
+
+    '''
+
+    osm_coords = list(osm_edge.coords)
+    ref_coords = list(ref_edge.coords)
+
+    hausdorff_dist = max(directed_hausdorff(osm_coords, ref_coords)[0], directed_hausdorff(ref_coords, osm_coords)[0])
+
+    return hausdorff_dist
+
+
+def clip_new_edge(line_to_split, split_line):
+
+    '''
+    Clips one LineString to the extent of another LineString
+
+    Due to limitations in coordinate precision in Shapely, the function uses a workaround where a new LineString is constructed based on clip points snapped to the original line.
+    (Rather than using the Shapely clip function, which in case of floating point imprecision will fail for many instances).
+
+    Parameters
+    ----------
+    line_to_split: Shapely LineString
+
+    split_line: Shapely LineString
+
+
+    Returns
+    -------
+    clipped_line: Shapely Linestring
+        First line clipped to the extent of the second line.
+
+    '''
+
+    start_node = Point(split_line.coords[0])
+    end_node = Point(split_line.coords[-1])
+
+    # Get nearest point on reference geometry to start and end nodes of OSM match
+    _, nearest_point_start = nearest_points(start_node, line_to_split)
+    _, nearest_point_end = nearest_points(end_node, line_to_split)
+        
+    new_nearest_start = snap(nearest_point_start, line_to_split, 0.01)
+    new_nearest_end = snap(nearest_point_end, line_to_split, 0.01)
+
+    clipped_line = LineString( [new_nearest_start, new_nearest_end] )
+
+    return clipped_line
+
 
 def find_matches_buffer(reference_data, osm_data, col_ref_id, dist):
 
     '''
-    Parameters are: 
-        - GeoDataFrame with edges/linestrings to be matched to OSM data
-        - GeoDataFrame with OSM edges that reference data should be matched to
-        - The name of the column with unique ID for reference features
-        - The max distance between features that should be considered a match
+    Function for finding which OSM edges intersect a buffered reference data set.
+    The first step in a matching of OSM with another line data set.
+
+    Parameters
+    ----------
+    reference_data: GeoDataFrame (geopandas)
+        GDF with edges (LineStrings) to be matched to OSM edges.
+
+    osm_data: GeoDataFrame (geopandas)
+        GeoDataFrame with OSM edges which the reference data should be matched to.
+
+    col_ref_id: String
+        Name of column with unique ID for reference feature
+
+    dist: Numerical
+        Max distance between distances that should be considered a match (used for creating the buffers)
+
+
+    Returns
+    -------
+    matches DataFrame (pandas):
+        DataFrame with the reference index as index, a column with reference data unique ID and the index and ID of intersecting OSM edges.
+
 
     '''
 
@@ -93,35 +226,49 @@ def find_matches_buffer(reference_data, osm_data, col_ref_id, dist):
     return matches
 
 
-def get_hausdorff_dist(osm_edge, ref_edge):
+def save_best_match(osm_index, ref_index, ref_id_col, row, potential_matches, final_matches, partially_matched, clipped_edges, pct_removed_threshold, meters_removed_threshold):
 
     '''
-    Computes the Hausdorff distance (max distance) between two LineStrings
-    '''
+    Function for saving the best of the potential matches.
+    To be used internally in the function for finding the exact match between features in reference and OSM data.
+    Also check how much of the final match is clipped/removed for the match, and checks for full/partial match.
 
-    osm_coords = list(osm_edge.coords)
-    ref_coords = list(ref_edge.coords)
+    Parameters
+    ----------
+    osm_index: index key (string or int)
+        Index of matched OSM feature
 
-    hausdorff_dist = max(directed_hausdorff(osm_coords, ref_coords)[0], directed_hausdorff(ref_coords, osm_coords)[0])
+    ref_index: index key (string or int)
+        Index of matched reference feature
 
-    return hausdorff_dist
+    ref_id_col: string
+        Name of column with unique ID for reference data
 
+    row: pandas row
+        Row in matched reference data currently being looked at
 
-def save_best_match(osm_index, ref_index, ref_id_col, row, potential_matches, final_matches, partially_matched, clipped_edges, pct_removed_threshold = None, meters_removed_threshold = None):
+    potential_matches: 
+        Dataframe with OSM edges that are potential matches
 
-    '''
-    Parameters:
-        - index of matched osm feature 
-        - index of matched reference_data feature
-        - name of column with ref unique id
-        - row in reference_data data that is being matched
-        - dataframe with osm matches (potential/best matches)
-        - thresholds for pct/meters removed
-        - Dataframes used for: 
-            - final_matches
-            - partially matched
-            - osm_matches
-            - clipped_edges
+    final_matches: pandas DataFrame
+        DataFrame used to store final mathces
+
+    partially_matched: pandas DataFrame
+        DataFrame used to store partial matches
+
+    clipped_edges: pandas DataFrame
+        DataFrame storing how much and to what reference edges have been clipped
+
+    pct_removed_threshold: float/int
+        Threshold for how many percent of an edge can be clipped before it is considered only partially matched
+
+    meters_removed_threshold: float/int
+        Threshold for how many meters of an edge can be clipped before it is considered only partially matched
+
+    Returns
+    -------
+    None:
+        Updates dataframe with final matches
     '''
 
     final_matches.at[ref_index, ref_id_col] = row[ref_id_col]
@@ -139,30 +286,43 @@ def save_best_match(osm_index, ref_index, ref_id_col, row, potential_matches, fi
         partially_matched.at[ref_index, 'pct_removed'] = clipped_edges.loc[osm_index, 'meters_removed']
 
 
-def clip_new_edge(line_to_split, split_line):
-
-    '''
-    Function for clipping one LineString to the extent of another LineString
-
-    Due to coordinate imprecision and limitations in shapely split-function, this workaround is needed
-    '''
-
-    start_node = Point(split_line.coords[0])
-    end_node = Point(split_line.coords[-1])
-
-    # Get nearest point on reference geometry to start and end nodes of OSM match
-    _, nearest_point_start = nearest_points(start_node, line_to_split)
-    _, nearest_point_end = nearest_points(end_node, line_to_split)
-        
-    new_nearest_start = snap(nearest_point_start, line_to_split, 0.01)
-    new_nearest_end = snap(nearest_point_end, line_to_split, 0.01)
-
-    clipped_line = LineString( [new_nearest_start, new_nearest_end] )
-
-    return clipped_line
-
-
 def find_exact_matches(matches, osm_edges, reference_data, ref_id_col, angular_threshold=20, hausdorff_threshold=15, pct_removed_threshold=20, meters_removed_threshold=5, crs='EPSG:25832'):
+
+    '''
+    Parameters
+    ----------
+    matches: pandas DataFrame
+        Dataframe with potential OSM matches for each reference features (based on intersection of buffered reference geometries)
+
+    osm_edges: geopandas DataFrame
+        OSM edges to be matched
+
+    reference_data: geopandas DataFrame
+        reference edges to be matched to OSM
+
+    ref_id_col: String
+        Name of column with unique ID for reference data
+
+    angular_threshold: float/int
+        Threshold for angular difference between features than can be considered matches
+
+    hausdorff_threshold: float/int
+        Threshold for Hausdorff distance between features than can be considered matches
+
+    pct_removed_threshold: float/int
+        Threshold for how many pct of the length can be cut from reference edge before it is only considered partially matched.
+
+    meters_removed_threshold: float/int
+        Threshold for how many meters can be cut from reference edge before it is only considered partially matched.
+
+    Returns
+    -------
+    final_matches: pandas DataFrame
+        DataFrame with the final matches of reference and OSM data
+
+    ref_part_matches: pandas DataFrame
+        Dataframe with reference edges that have only been partially matched
+    '''
 
     ref_part_matched = gpd.GeoDataFrame(index = reference_data.index, columns=[ref_id_col, 'meters_removed','pct_removed','geometry'], crs=crs)
     final_matches = gpd.GeoDataFrame(index = reference_data.index, columns = [ref_id_col,'osmid','osm_index','geometry'], crs=crs)
@@ -178,8 +338,6 @@ def find_exact_matches(matches, osm_edges, reference_data, ref_id_col, angular_t
 
         # If no matches exist at all, continue to next reference_data geometry and add the reference_data feature as unmatched
         if len(row.matches_index) < 1:
-
-            #ref_not_matched.append(row[ref_id_col])
 
             continue
 
@@ -236,8 +394,6 @@ def find_exact_matches(matches, osm_edges, reference_data, ref_id_col, angular_t
 
             if len(potential_matches) == 0:
 
-                #ref_not_matched.append(row[ref_id_col])
-
                 continue
             
             elif len(potential_matches) == 1:
@@ -281,6 +437,10 @@ def find_exact_matches(matches, osm_edges, reference_data, ref_id_col, angular_t
     print(f'{ len(reference_data) - len(final_matches) } reference edges where not matched')
     
     return final_matches, ref_part_matched
+
+
+
+
     
 ###########################################
 
@@ -357,3 +517,20 @@ def split_edge(line_to_split, split_line):
         clipped_lines_geoms = split(line_to_split, clip_points).geoms
 
     return clipped_lines_geoms
+
+
+########################
+
+if __name__ == '__main__':
+    pass
+
+
+# Test get_geom_diff
+
+# Test get_angle
+
+# Test get_hausdorff_dist
+
+# Test clip_new_edge
+
+# Get test data and check higher order functions (this data can also be used for quality check?)
