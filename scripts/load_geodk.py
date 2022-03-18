6854ae1c9b17c078d1cafc9d5e8b97670506e9e6
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-03-18 10:51:07 +0100

    simplified geodk

--- scripts/load_geodk.py
+++ scripts/load_geodk.py
@@ -8,6 +8,9 @@ import geopandas as gpd
 import yaml
 from src import db_functions as dbf
 import pickle
+from src import matching_functions as mf
+import osmnx as ox
+from src import simplification_functions as sf
 # %%
 with open(r'config.yml') as file:
     parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
@@ -42,6 +45,45 @@ geodk = geodk.to_crs(crs)
 
 assert geodk.crs == crs
 #%%
+# Create graph structure
+graph_ref = mf.create_osmnx_graph(geodk)
+
+# Works, but changes some edge geometries??
+G_sim = sf.simplify_graph(graph_ref, attributes = ['vejklasse'])
+
+#%%
+reg_nodes, reg_edges = ox.graph_to_gdfs(graph_ref)
+
+sim_nodes, sim_edges = ox.graph_to_gdfs(G_sim)
+
+sim_nodes.to_file('../data/sim_nodes.gpkg', driver='GPKG')
+sim_edges[['geometry','vejklasse']].to_file('../data/sim_edges.gpkg', driver='GPKG')
+reg_nodes.to_file('../data/reg_nodes.gpkg', driver='GPKG')
+reg_edges.to_file('../data/red_edges.gpkg', driver='GPKG')
+
+#%%
+# Check crs
+nodes, edges = ox.graph_to_gdfs(G_sim)
+
+assert edges.crs == crs, 'Data is in wrong crs!'
+
+#%%
+
+# Create unique id
+edges['old_id'] = edges.fot_id
+
+edges.reset_index(inplace=True)
+
+ids = []
+for i in range(1000, 1000+len(edges)):
+    ids.append(i)
+
+edges['new_id'] = ids
+
+assert len(edges.new_id.unique()) == len(edges)
+
+#%%
+
 if use_postgres:
 
     connection = dbf.connect_pg(db_name, db_user, db_password)
