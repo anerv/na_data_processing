1a4af0328a3fa8e99528e18370f08390b1eb6264
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-03-21 17:48:08 +0100

    done

--- scripts/match_networks.py
+++ scripts/match_networks.py
@@ -1,14 +1,9 @@
 '''
 Script for matching road networks.
-Method assumes a simplified version of public dataset and a non-simplified version of OSM (osm_i.e. that edges in the reference_data dataset usually are as long or longer than OSM).
-Use case is situations where a specific attribute is poorly defined in OSM, and the script is thus developed for a situation where it is assumed that most core road geometries are present in both data sets.
-But it is also possible to use for identifying edges in the reference_data dataset with no match in OSM or vice-versa.
-
 '''
 # TODO: Docs
 
 # TODO: Functionality for doing analysis grid by grid
-
 
 #%%
 import pickle
@@ -19,7 +14,7 @@ from src import db_functions as dbf
 from src import matching_functions as mf
 import osmnx as ox
 import matplotlib.pyplot as plt
-
+import os.path
 #%%
 with open(r'config.yml') as file:
     parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
@@ -37,7 +32,7 @@ with open(r'config.yml') as file:
     quality_check = parsed_yaml_file['quality_check']
     quality_data = parsed_yaml_file['quality_data']
 
-    add_unmatched = parsed_yaml_file['add_unmatched']
+    org_ref_id_col = parsed_yaml_file['org_ref_id_col']
   
 print('Settings loaded!')
 
@@ -98,50 +93,84 @@ print(f'Number of rows in osm_edge table: {len(osm_edges)}')
 print(f'Number of rows in reference_data table: {len(reference_data)}')
 
 #%%
-ref = gpd.read_file('../tests/geodk_small_test.gpkg')
-osm = gpd.read_file('../tests/osm_small_test.gpkg')
+#ref = gpd.read_file('../tests/geodk_small_test.gpkg')
+#osm = gpd.read_file('../tests/osm_small_test.gpkg')
+#%%
+if os.path.exists('../data/ref_segments_full.gpkg'):
+    ref_segments = gpd.read_file('../data/ref_segments_full.gpkg')
+
+else:
+    # Create segments
+    ref_segments = mf.create_segment_gdf(reference_data, segment_length=10, id_col=org_ref_id_col)
+    ref_segments.set_crs(crs, inplace=True)
+    ref_segments.to_file('../data/ref_segments_full.gpkg', driver='GPKG')
 
 #%%
-# Create segments
-ref_segments = mf.create_segment_gdf(reference_data, segment_length=10, id_col='fot_id')
-print('first done')
-#%%
-# Save to file
-ref_segments.set_crs(crs, inplace=True)
-ref_segments.to_file('../data/ref_segments_full.gpkg', driver='GPKG')
-#%%
-# TODO: Optimize! Only segmentize OSM edges within buffer distance from reference edges!
-# Use buffer and sindex
-osm_segments = mf.create_segment_gdf(osm_edges, segment_length=10, id_col='osmid')
-osm_segments['old_osmid'] = osm_segments.osmid
-osm_segments.osmid = osm_segments.seg_id
-osm_segments.set_crs(crs, inplace=True)
-osm_segments.to_file('../data/osm_segments_full.gpkg', driver='GPKG')
+if os.path.exists('../data/osm_segments_full.gpkg'):
+    osm_segments = gpd.read_file('../data/osm_segments_full.gpkg')
+
+else:
+    # Only segmentize OSM edges within buffer distance from reference edges!
+    ref_buffer = reference_data.copy()
+    ref_buffer.geometry = ref_buffer.geometry.buffer(distance=15)
+    osm_sindex = osm_edges.sindex
+
+    matches_ix = []
+
+    for index, row in ref_buffer.iterrows():
+            buffer = row['geometry']
+            possible_matches_index = list(osm_sindex.intersection(buffer.bounds))
+            possible_matches = osm_edges.iloc[possible_matches_index]
+            precise_matches = possible_matches[possible_matches.intersects(buffer)]
+            precise_matches_index = list(precise_matches.index)
+            matches_ix.extend(precise_matches_index)
+
+    matches_unique = list(set(matches_ix))
+    osm_subset = osm_edges.loc[matches_unique]
+
+    osm_segments = mf.create_segment_gdf(osm_subset, segment_length=10, id_col='osmid')
+    osm_segments['org_osmid'] = osm_segments.osmid
+    osm_segments.rename(columns={'seg_id':'osmid'}) # Because function assumes an id column names osmid
+    osm_segments.set_crs(crs, inplace=True)
+    osm_segments.to_file('../data/osm_segments_full.gpkg', driver='GPKG')
 
 #%%
-final_matches_test = mf.find_matches_segments(osm_edges=osm_segments, reference_data=ref_segments, ref_id_col='seg_id', buffer_dist=10)
+# Get smaller subsets
+xmin = 723371
+xmax = xmin + 2000
+ymin = 6180833
+ymax = ymin + 2000
+
+osm_segments = osm_segments.cx[xmin:xmax, ymin:ymax].copy(deep=True)
+ref_segments = ref_segments.cx[xmin:xmax, ymin:ymax].copy(deep=True)
+
 #%%
-id_matches = final_matches_test.seg_id.to_list()
+buffer_matches = mf.find_matches_buffer(reference_data=ref_segments, osm_data=osm_segments, ref_id_col='seg_id', dist=15)
+#%%
+final_matches = mf.find_matches_from_buffer(buffer_matches=buffer_matches, osm_edges=osm_segments, reference_data=ref_segments, ref_id_col='seg_id', hausdorff_threshold=17)
+#%%
+osm_updated = mf.update_osm(osm_segments=osm_segments, ref_segments=ref_segments, osm_data=osm_edges, reference_data=reference_data, final_matches=final_matches, attr='vejklasse',org_ref_id_col=org_ref_id_col)
+
+#%%
+
+'''
+id_matches = final_matches.seg_id.to_list()
 matched = ref_segments.loc[ref_segments.seg_id.isin(id_matches)]
 not_matched = ref_segments.loc[~ref_segments.seg_id.isin(id_matches)]
 
-matched = matched.merge(final_matches_test, on='seg_id')
-matched = matched.astype({'fot_id':'int32','seg_id':'int32','osmid':'int32','osm_index':'int32'})
+matched = matched.merge(final_matches, on='seg_id')
+matched = matched.astype({org_ref_id_col:'int64','seg_id':'int64','osmid':'int64','osm_index':'int64'})
+#
+osm_id_matches = final_matches.osmid.to_list()
+osm_matched = osm_segments.loc[osm_segments.osmid.isin(osm_id_matches)]
+osm_matched.to_file('../data/osm_matched.gpkg', driver='GPKG')
 
 matched.to_file('../data/matched.gpkg', driver='GPKG')
 not_matched.to_file('../data/not_matched.gpkg', driver='GPKG')
 
-#%%
-# TODO: Reassemble!
-
-def reassemble_segments():
-
-    # Merge segments with same ID
-
-    # Convert other attribute (i.e. which line they were matched to as list)
-    # only keep unique values in list
-
-    pass
+ref_segments.to_file('../data/ref_segments_test.gpkg', driver='GPKG')
+osm_segments.to_file('../data/osm_segments_test.gpkg', driver='GPKG')
+'''
 
 #%% 
 # TODO: Quality check (optional)
@@ -155,118 +184,22 @@ if quality_check:
     pass
 
 #%%
-# TODO: Add unmatched to dataset
-# Not just a question of adding to database - should create uniform col names, geometric structure (i.e. simplified or not)
-# Unmatched are those not matched when rerunning partial matches
-
-# TODO: Change to actual unmatched - this is just for testing
-unmatched = None
-
-if add_unmatched:
-    
-   
-    # Run function for adding data
-
-    # How will this work on a grid by grid basis? Maybe not at all?
-    # Test for a city and then for increasing areas
-
-    pass
-
-
-#%%
-import networkx as nx
-
-col_dictionary = {
-    'vejklasse': 'highway',
-    'overflade': 'surface'
-}
-
-# niveau can indicate bridge or tunnel
-# trafikart can indicate whether there are motortraffic or not
-
-def add_unmatched_data(osm_edges, osm_nodes, unmatched_edges):
-
-    # Should check whether the unmatched network is of the same graph type as OSM network
-
-    # Assert that they are the same reference system
-
-    if osm_edges.crs != unmatched_edges.crs:
-
-        unmatched_edges = unmatched_edges.to_crs(osm_edges.crs)
-
-        assert osm_edges.crs == unmatched_edges.crs
-    
-    # Run function for converting to osmnx format
-    unmatched_graph = mf.create_osmnx_graph(unmatched_edges)
-
-    # Create new graph object from OSM edges
-
-    # Read nodes
-
-    # Recreate multiindex
-
-    osm_graph = ox.graph_from_gdfs(osm_nodes, osm_edges)
-
-    assert unmatched_graph.crs == osm_graph.crs, 'CRS do not match!'
-
-
-    combined_graph = None
-
-    return combined_graph
-
-    
-#%%
 #######################################################
+if use_postgres:
 
-# Upload result to DB
-engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
+    # Upload result to DB
+    engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 
-not_matched = None
+    dbf.to_postgis(geodataframe=osm_updated, table_name='osm_updated', engine=engine)
 
-dbf.to_postgis(geodataframe=not_matched, table_name='not_matched', engine=engine)
+    dbf.to_postgis(geodataframe=final_matches, table_name='final_matches', engine=engine)
 
-connection.close()
+    connection.close()
 
+else:
+    with open('../data/osm_reference_match.pickle', 'wb') as handle:
+        pickle.dump(final_matches, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open('../data/osm_updated.pickle', 'wb') as handle:
+        pickle.dump(osm_updated, handle, protocol=pickle.HIGHEST_PROTOCOL)
 #%%
-osm_edges[['osmid','geometry']].to_file('../data/osm_subset.gpkg', driver='GPKG')
-
-reference_data.to_file('../data/ref_subset.gpkg', driver='GPKG')
-
-#%%
-fm = final_matches.copy(deep=True)
-
-fm.reset_index(inplace=True, drop=True)
-
-fm[["fot_id", "osmid",'osm_index']] = fm[["fot_id", "osmid",'osm_index']].apply(pd.to_numeric)
-
-#pm = partially_matched.copy(deep=True)
-
-#pm.reset_index(inplace=True, drop=True)
-
-#pm[["fot_id", "osmid",'osm_index']] = pm[["fot_id", "osmid",'osm_index']].apply(pd.to_numeric)
-
-# Save results
-fm.to_file('../data/osm_reference_match.gpkg', layer='final_matches', driver='GPKG')
-#pm.to_file('../data/osm_reference_match.gpkg', layer='partially_matched', driver='GPKG')
-
-#%%
-updated_osm.to_file('../data/osm_reference_match.gpkg', layer='updated_osm', driver='GPKG')
-#%%
-with open('../data/osm_reference_match.pickle', 'wb') as handle:
-    pickle.dump(final_matches, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open('../data/buffer_matches.pickle', 'wb') as handle:
-    pickle.dump(matches, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open('../data/partial_matches.pickle', 'wb') as handle:
-    pickle.dump(partially_matched, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open('../data/updated_osm.pickle', 'wb') as handle:
-    pickle.dump(updated_osm, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#%%
-#############################################
-
-
-# %%
-
-
