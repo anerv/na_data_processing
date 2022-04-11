4a1e3b14653396e90414b77c2334f07efb5b6eb8
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-04-11 13:01:31 +0200

    grid func works (but slow)

--- scripts/match_networks.py
+++ scripts/match_networks.py
@@ -119,14 +119,13 @@ else:
     osm_segments[['highway','osmid','org_osmid','geometry']].to_file('../data/osm_segments_full.gpkg', driver='GPKG')
 
 #%%
-# TODO: Functionality for doing analysis grid by grid!!
-%%time
-
 # Create grid and buffered grid
 grid = mf.create_grid_bounds(ref_segments, 1000)
 grid['grid_id'] = grid.index
 buffered_grid = grid.copy(deep=True)
-buffered_grid.geometry = buffered_grid.geometry.buffer(50)
+buffered_grid.geometry = buffered_grid.geometry.buffer(10)
+
+# TODO Speed up by doing overlay before segmentizing?
 
 # Assign grid index to data
 ref_grid = gpd.overlay(ref_segments, grid, how='intersection', keep_geom_type=False)
@@ -138,10 +137,10 @@ osm_grid_buffered = gpd.overlay(osm_segments, buffered_grid, how='intersection',
 #%%
 %%time
 ref_id_col = 'seg_id'
-grid_ids = grid.grid_id.to_list()
-grid_ids = grid_ids[0:50]
+grid_ids = list(ref_grid.grid_id.unique())
+grid_ids = grid_ids[0:5]
 
-results = []
+results = {}
 
 for g in grid_ids:
 
@@ -169,13 +168,16 @@ for g in grid_ids:
         if len(buffer_matches) > 0:
             final_matches = mf.find_matches_from_buffer(buffer_matches=buffer_matches, osm_edges=o_seg_b, reference_data=r_seg_b, angular_threshold=30, hausdorff_threshold=17)
             print('Final matches found!')
+
             # Only keep results from those in regular grid
             final_matches = final_matches.loc[final_matches[ref_id_col].isin(ref_id_list)]
 
             ids_attr_dict = mf.summarize_matches(o_seg, final_matches, 'vejklasse')
 
-            # TODO: Find better way of saving results!
-            results.append(ids_attr_dict)
+            # Not interested in none values
+            ids_attr_dict = {key:val for key, val in ids_attr_dict.items() if val != 'none'}
+
+            results = dict(results, **ids_attr_dict)
 
             print('One cell analysed!')
 
@@ -195,10 +197,51 @@ ref_segments = ref_segments.cx[xmin:xmax, ymin:ymax].copy(deep=True)
 buffer_matches = mf.overlay_buffer(reference_data=ref_segments, osm_data=osm_segments, ref_id_col='seg_id', dist=15)
 #%%
 final_matches = mf.find_matches_from_buffer(buffer_matches=buffer_matches, osm_edges=osm_segments, reference_data=ref_segments, angular_threshold=30, hausdorff_threshold=17)
-#%%
-osm_updated = mf.update_osm(osm_segments=osm_segments, osm_data=osm_edges, final_matches=final_matches, attr='vejklasse')
+
+#osm_updated = mf.update_osm(osm_segments=osm_segments, osm_data=osm_edges, final_matches=final_matches, attr='vejklasse')
 
 #%%
+
+def summarize_matches(osm_segments, final_matches, attr):
+
+    '''
+    Creates a dictionary with the original feature ids and the attribute they have been matched to
+
+    Arguments:
+        osm_segments (geodataframe): osm_segments used in the analysis
+        final_matches: reference_data with information about corresponding osm segments
+        attr (str): name of column in final_matches data with attribute to be transfered to osm data
+
+    '''
+
+    # TODO: Prevent conversion to float of indices and ids?
+    #Create dataframe with new and old ids and information on matches
+    final_matches['osmid'] = final_matches['matches_id']
+    osm_merged = osm_segments.merge(final_matches.drop('geometry',axis=1), how='left', on='osmid')
+    
+    org_ids = list(osm_merged['org_osmid'].unique())
+
+    matched_attributes = {}
+
+    for i in org_ids:
+        
+        feature = osm_merged.loc[osm_merged.org_osmid == i].copy(deep=True)
+        feature[attr] = feature[attr].fillna('none')
+
+        matched_values = feature[attr].unique()
+        if len(matched_values) == 1:
+            matched_attributes[i] = matched_values[0]
+
+        else:
+            feature['length'] = feature.geometry.length
+            summed = feature.groupby(attr).agg({'length': 'sum'})
+            majority_value = summed['length'].idxmax()
+            matched_attributes[i] = majority_value
+
+    return matched_attributes 
+
+#%%
+
 '''
 # Export matches
 # Get matched and non-matched segments
@@ -252,5 +295,17 @@ else:
 
     with open('../data/osm_updated.pickle', 'wb') as handle:
         pickle.dump(osm_updated, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#%%
+%load_ext line_profiler
+#%%
+stats_lprun = %lprun -r -f mf.find_best_match mf.find_best_match()
+# %%
+stats_lprun = %lprun -r -f mf.find_best_match mf.find_matches_from_buffer(buffer_matches=buffer_matches, osm_edges=osm_segments, reference_data=ref_segments, angular_threshold=30, hausdorff_threshold=17)
 
 #%%
+%prun estimate_pi()
+#%%
+%prun -s cumulative estimate_pi()
+#%%
+import line_profiler
+# %%
