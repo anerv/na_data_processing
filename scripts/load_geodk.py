8b6badb73cbc6516df8e10cf212ccfda9feb1c7f
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-08-15 18:42:33 +0200

    done

--- scripts/load_geodk.py
+++ scripts/load_geodk.py
@@ -8,8 +8,9 @@ import yaml
 from src import db_functions as dbf
 import pickle
 # from src import matching_functions as mf
-# import osmnx as ox
-# from src import simplification_functions as sf
+import osmnx as ox
+from src import graph_functions as gf
+from src import simplification_functions as sf
 # %%
 with open(r'config.yml') as file:
     parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
@@ -47,30 +48,33 @@ assert geodk.crs == crs
 assert len(geodk) == len(geodk[geodk_id_col].unique())
 
 #%%
-# Create graph structure
-#graph_ref = mf.create_osmnx_graph(geodk)
+# Get cycling infrastructure
+geodk_bike = geodk.loc[geodk.vejklasse.isin(['Cykelsti langs vej','Cykelbane langs vej'])].copy()
 
 #%%
-# G_sim = sf.momepy_simplify_graph(graph_ref, attributes=['vejklasse'])
+# Create graph structure
+graph_ref = gf.create_osmnx_graph(geodk_bike)
+
+#%%
+G_sim = sf.momepy_simplify_graph(graph_ref, attributes=['vejklasse'])
+
+# Check crs
+nodes, edges = ox.graph_to_gdfs(G_sim)
+assert edges.crs == crs, 'Data is in wrong crs!'
 
 # #%%
-# # Check crs
-# nodes, edges = ox.graph_to_gdfs(G_sim)
-# assert edges.crs == crs, 'Data is in wrong crs!'
+# Create unique id
+edges['old_id'] = edges.fot_id
 
-# #%%
-# # Create unique id
-# edges['old_id'] = edges.fot_id
+edges.reset_index(inplace=True)
 
-# edges.reset_index(inplace=True)
+ids = []
+for i in range(1000, 1000+len(edges)):
+    ids.append(i)
 
-# ids = []
-# for i in range(1000, 1000+len(edges)):
-#     ids.append(i)
+edges['edge_id'] = ids
 
-# edges['new_id'] = ids
-
-# assert len(edges.new_id.unique()) == len(edges)
+assert len(edges.edge_id.unique()) == len(edges)
 
 #%%
 if use_postgres:
@@ -79,26 +83,30 @@ if use_postgres:
 
     engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 
-    dbf.to_postgis(geodataframe=geodk, table_name='vm_brudt', engine=engine)
-    # dbf.to_postgis(geodataframe=edges, table_name='vm_brudt_simple', engine=engine)
-    # dbf.to_postgis(geodataframe=nodes, table_name='vm_brudt_nodes_simple', engine=engine)
+    dbf.to_postgis(geodataframe=geodk_bike, table_name='geodk_bike', engine=engine)
+    dbf.to_postgis(geodataframe=edges, table_name='geodk_bike_simple', engine=engine)
+    dbf.to_postgis(geodataframe=nodes, table_name='geo_dk_nodes_simple', engine=engine)
 
-    q = 'SELECT fot_id, feat_type FROM vm_brudt LIMIT 10;'
-    #q2 = 'SELECT fot_id, feat_type FROM vm_brudt_simple LIMIT 10;'
+    q = 'SELECT fot_id, feat_type FROM geodk_bike LIMIT 10;'
+    q2 = 'SELECT fot_id, feat_type FROM geodk_bike_simple LIMIT 10;'
 
     test = dbf.run_query_pg(q, connection)
 
     print(test)
 
-    # test2 = dbf.run_query_pg(q2, connection)
+    test2 = dbf.run_query_pg(q2, connection)
 
-    # print(test2)
+    print(test2)
 
     connection.close()
 
 else:
  
     with open('../data/reference_data.pickle', 'wb') as handle:
-        pickle.dump(geodk, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(graph_ref, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open('../data/reference_data_simple.pickle', 'wb') as handle:
+        pickle.dump(G_sim, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
 
 # %%
