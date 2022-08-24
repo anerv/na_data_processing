a04bd957bbf375ef2a74d323d8cb4d0cccfbd34c
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-08-24 14:40:37 +0200

    notes for defining cycling network

---
+++ scripts/sql/create_cycling_network.sql
@@ -0,0 +1,10 @@
+
+remove all where bicycle is no, dismount, private
+remove all where access is private
+remove all highways where bicycle infrastructure is not yes
+ "highway" = 'footway' and bicycle IN ('designated','permissive','yes','destination' 
+ bicycle IN ('yes','permissive', 'ok', 'allowed', 'designated')
+
+ remove all where highway is pedestrian and...
+
+remove unconnected components less than a certain size
