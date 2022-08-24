ee6dfb005c61f4b72803af99bbe7234926e2f4d7
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-08-24 16:55:50 +0200

    added notes

---
+++ scripts/sql/lts_classifications.sql
@@ -0,0 +1,5 @@
+-- classify edges leading to unmarked intersections with high stress streets
+
+-- CLASSIFY EDGES BASED ON THEIR NODES
+    -- WHAT TYPE OF INTERSECTION DO THEY CONNECT TO
+    -- WHAT CLASS OF STREET DO THEY CONNECT TO
