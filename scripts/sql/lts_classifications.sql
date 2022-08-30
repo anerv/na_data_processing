8db08ca81f7ee1ad126496a83eba9abce8476ba8
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-08-30 15:20:10 +0200

    notes added

--- scripts/sql/lts_classifications.sql
+++ scripts/sql/lts_classifications.sql
@@ -1,5 +1,11 @@
 -- classify edges leading to unmarked intersections with high stress streets
 
+-- class 1 - dedicated, protected cycling infra
+-- class 2 - unprotected cycling infra
+-- class 3 - ....
+-- class 4 - ...
+
 -- CLASSIFY EDGES BASED ON THEIR NODES
     -- WHAT TYPE OF INTERSECTION DO THEY CONNECT TO
     -- WHAT CLASS OF STREET DO THEY CONNECT TO
+
