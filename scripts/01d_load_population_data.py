464202a847bbed81702136f218c6d6988e1de800
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-09-01 16:22:47 +0200

    renamed files

---
+++ scripts/01d_load_population_data.py
@@ -0,0 +1,7 @@
+'''
+Script should load population data to database??
+Create H3 polygons with population data
+Classify as urban/non-urban
+Save H3 polygons to be used to index network for analysis
+Save urban/non-urban in a way that allows for classifying network
+'''
