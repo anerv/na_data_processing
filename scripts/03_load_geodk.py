1ca70c7eeba9d54e0cd7e6393f9aa7fe3515be95
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-09-01 16:17:44 +0200

    removed graph part

--- scripts/03_load_geodk.py
+++ scripts/03_load_geodk.py
@@ -6,10 +6,6 @@ Required an existing db with postgis extension
 import geopandas as gpd
 import yaml
 from src import db_functions as dbf
-import pickle
-import osmnx as ox
-from src import graph_functions as gf
-from src import simplification_functions as sf
 # %%
 with open(r'config.yml') as file:
     parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
@@ -50,61 +46,26 @@ assert len(geodk) == len(geodk[geodk_id_col].unique())
 # Get cycling infrastructure
 geodk_bike = geodk.loc[geodk.vejklasse.isin(['Cykelsti langs vej','Cykelbane langs vej'])].copy()
 
-#%%
+# Create unique edge id column
 geodk_bike['edge_id'] = geodk_bike.fot_id
 
 assert len(geodk_bike.edge_id) == len(geodk_bike)
 #%%
-# Create graph structure
-
-# graph_ref = gf.create_osmnx_graph(geodk_bike)
-
-# #%%
-# G_sim = sf.momepy_simplify_graph(graph_ref, attributes=['vejklasse'])
-
-# # Check crs
-# nodes, edges = ox.graph_to_gdfs(G_sim)
-# assert edges.crs == crs, 'Data is in wrong crs!'
-
-# # #%%
-# # Create unique id
-# edges['old_id'] = edges.fot_id
-
-# edges.reset_index(inplace=True)
-
-# ids = []
-# for i in range(1000, 1000+len(edges)):
-#     ids.append(i)
-
-# edges['edge_id'] = ids
-
-# assert len(edges.edge_id.unique()) == len(edges)
-
-#%%
-
 connection = dbf.connect_pg(db_name, db_user, db_password)
 
 engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 
 dbf.to_postgis(geodataframe=geodk_bike, table_name='geodk_bike', engine=engine)
-# dbf.to_postgis(geodataframe=edges, table_name='geodk_bike_simple', engine=engine)
-# dbf.to_postgis(geodataframe=nodes, table_name='geo_dk_nodes_simple', engine=engine)
 
 q = 'SELECT edge_id, vejklasse FROM geodk_bike LIMIT 10;'
-#q2 = 'SELECT fot_id, feat_type FROM geodk_bike_simple LIMIT 10;'
 
 test = dbf.run_query_pg(q, connection)
 
 print(test)
 
-# test2 = dbf.run_query_pg(q2, connection)
-
-# print(test2)
-
 connection.close()
 
 #%%
- 
 # with open('../data/reference_data.pickle', 'wb') as handle:
 #     pickle.dump(graph_ref, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
