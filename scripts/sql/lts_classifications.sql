a4a1cc43891fd6a5f59b94054128debf5bd7cd54
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-09-01 16:22:57 +0200

    notes

--- scripts/sql/lts_classifications.sql
+++ scripts/sql/lts_classifications.sql
@@ -1,4 +1,5 @@
--- classify edges leading to unmarked intersections with high stress streets
+
+-- CLASSIFY EDGES BASED ON THEIR NODES
 
 -- class 1 - dedicated, protected cycling infra
 -- class 2 - unprotected cycling infra
@@ -10,4 +11,8 @@
     -- WHAT CLASS OF STREET DO THEY CONNECT TO
 
 
--- Find edges connected to type edges
+
+
+-- Classify nodes based on what type of edges they connect to
+-- If a node is connected to a high-stress street and it is unregulated: 
+--> all edges connnected to that node is assigned the higher stress? Or they are somehow changed so the intersection cannot be used/introduces a 'missing link'
