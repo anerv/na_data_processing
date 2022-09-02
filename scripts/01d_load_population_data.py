c93b7ee5783f98ddf4b7aa772eb78a649abb6064
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-09-02 10:46:13 +0200

    notes

--- scripts/01d_load_population_data.py
+++ scripts/01d_load_population_data.py
@@ -5,3 +5,25 @@ Classify as urban/non-urban
 Save H3 polygons to be used to index network for analysis
 Save urban/non-urban in a way that allows for classifying network
 '''
+
+#%%
+import h3
+import matplotlib.pyplot as plt
+
+#%%
+APERTURE_SIZE = 9
+hex_col = 'hex'+str(APERTURE_SIZE)
+
+# find hexs containing the points
+df[hex_col] = df.apply(lambda x: h3.geo_to_h3(x.lat,x.lng,APERTURE_SIZE),1)
+
+# calculate elevation average per hex
+df_dem = df.groupby(hex_col)['elevation'].mean().to_frame('elevation').reset_index()
+
+#find center of hex for visualization
+df_dem['lat'] = df_dem[hex_col].apply(lambda x: h3.h3_to_geo(x)[0])
+df_dem['lng'] = df_dem[hex_col].apply(lambda x: h3.h3_to_geo(x)[1])
+
+# plot the hexes
+plot_scatter(df_dem, metric_col='elevation', marker='o')
+plt.title('hex-grid: elevation');
